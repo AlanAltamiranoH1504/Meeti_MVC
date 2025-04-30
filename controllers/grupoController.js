2bf547c6a04ad498054402d090934a3ce2634fa1
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-04-30 17:11:31 -0600

    feat: add error handling in group saving
    - add errors according to wrong field in group saving
    
    - add alert function for errors or success in group saving

--- controllers/grupoController.js
+++ controllers/grupoController.js
@@ -13,7 +13,43 @@ const formNuevoGrupo = async (req, res) => {
 
 const saveGrupo = async (req, res) => {
     const {nombre, descripcion, categoria, url} = req.body;
+    const errores = [];
     const usuarioEnSesion = await userInSession(req.cookies.token_meeti);
+    /**
+     * AGREGAR CONFIHURACION DE MULTER Y MIDDLEWARE DE MULTER. EN EL FORMULARIO AGREGAR EL ACTION QUE
+     * UNICAMENTE SE ENCARGUE DE GUARDAR LA IMG ACTUALIZANDO EL GRUPO
+     */
+
+    if (nombre.trim() === "" || nombre == null){
+        const error = {
+            msg: "El nombre del grupo no puede estar vacio"
+        };
+        errores.push(error);
+    }
+    const grupoFindByNombre = await Grupo.findOne({where: {nombre}});
+    if (grupoFindByNombre){
+        const error = {
+            msg: "Ya existe un grupo con ese nombre"
+        }
+        errores.push(error);
+    }
+
+    if (descripcion.trim() === "" || descripcion == null){
+        const error = {
+            msg: "La descripcion del grupo no puede estar vacia"
+        }
+        errores.push(error);
+    }
+    if (categoria.trim() === "" || categoria== null){
+        const error = {
+            msg: "La categoria del grupo no puede estar vacia"
+        }
+        errores.push(error);
+    }
+
+    if (errores.length > 0){
+        return res.status(400).json(errores);
+    }
 
     try {
         const grupoSave = await Grupo.create({
@@ -24,6 +60,7 @@ const saveGrupo = async (req, res) => {
             url
         });
         const response = {
+            status: "200",
             msg: "Grupo creado correctamente"
         }
         return res.status(200).json(response);
--- public/js/nuevoGrupo.js
+++ public/js/nuevoGrupo.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const formNuevoGrupo = document.querySelector("#formNuevoGrupo");
     formNuevoGrupo.addEventListener("submit", peticionSaveNuevoGrupo);
 
-    function peticionSaveNuevoGrupo(e){
+    function peticionSaveNuevoGrupo(e) {
         e.preventDefault();
         const token = document.querySelector("meta[name='csrf-token']").getAttribute("content");
         const inputNombre = document.querySelector("#nombre").value;
@@ -26,10 +26,46 @@ document.addEventListener("DOMContentLoaded", () => {
         }).then((response) => {
             return response.json();
         }).then((data) => {
-            console.log(data)
-        }).catch((error) =>{
+            if (data.status === "200") {
+                mostrarAlertas("success", data.msg, null);
+            } else {
+                mostrarAlertas("error", "bad request", data);
+            }
+        }).catch((error) => {
             console.log("Error en peticion al backend");
             console.log(error.message);
-        })
+        });
+    }
+
+    function mostrarAlertas(tipo, msg, errores) {
+        limpiarInputs();
+        const divAlertas = document.querySelector("#divAlertas");
+        divAlertas.innerHTML = "";
+        if (tipo === "success") {
+            const parrafoAlerta = document.createElement("p");
+            parrafoAlerta.textContent = msg;
+            parrafoAlerta.classList.add("bg-success", "px-3", "py-2", "fw-semibold", "text-center", "rounded", "text-uppercase", "fs-5", "text-white");
+            divAlertas.appendChild(parrafoAlerta);
+            setTimeout(() => {
+                divAlertas.innerHTML = "";
+            }, 3000);
+        } else if (tipo === "error") {
+            errores.forEach((error) => {
+                const parrafoError = document.createElement("p");
+                parrafoError.textContent = error.msg;
+                parrafoError.classList.add("bg-danger", "px-3", "py-2", "fw-semibold", "text-center", "rounded", "text-uppercase", "fs-5", "text-white", "my-3");
+                divAlertas.appendChild(parrafoError);
+            });
+            setTimeout(() => {
+                divAlertas.innerHTML = "";
+            }, 4000);
+        }
+    }
+
+    function limpiarInputs() {
+        document.querySelector("#nombre").value = "";
+        document.querySelector("#x").value = "";
+        document.querySelector("#categorias").value = "";
+        document.querySelector("#sitio-web").value = "";
     }
 });
