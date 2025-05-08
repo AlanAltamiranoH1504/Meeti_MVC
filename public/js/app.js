fbb86cdebd5f7e0df9793c3c1602e9616e2a0cfc
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-05-07 22:30:14 -0600

    feat: add PIN enablement and free movement on leaflet map

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,9 @@ import {OpenStreetMapProvider} from "leaflet-geosearch";
 
 const lat = 19.2920813;
 const lng = -99.0468341;
-
 const map = L.map('mapa').setView([lat, lng], 15);
+let marker;
+
 document.addEventListener("DOMContentLoaded", () => {
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -14,12 +15,22 @@ document.addEventListener("DOMContentLoaded", () => {
     buscador.addEventListener("input", buscarDireccion);
 });
 
-function buscarDireccion(e){
-    if (e.target.value.length > 8){
+function buscarDireccion(e) {
+    if (e.target.value.length > 8) {
         //Utilizamos el provider
         const provider = new OpenStreetMapProvider();
         provider.search({query: e.target.value}).then((resultado) => {
-            console.log(resultado)
+            //Mostrar el mapa en la ubicacion ingresada
+            map.setView(resultado[0].bounds[0], 15);
+
+            //Agregamos pin de ubicacion ingresada
+            marker = new L.marker(resultado[0].bounds[0], {
+                draggable: true, //Movimiento del PIN
+                autoPan: true
+            })
+                .addTo(map)
+                .bindPopup(resultado[0].label)
+                .openPopup();
         })
     }
 }
