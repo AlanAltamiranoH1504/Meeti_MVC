3545ba4c39dd910b0e2c6a09c12799d40e0d0f49
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-05-17 17:21:29 -0600

    feat: add filtering of meetis by group category - add meetis filtering by category and event date

--- controllers/categoriaController.js
+++ controllers/categoriaController.js
@@ -1,18 +1,36 @@
-import {Grupo} from "../models/index.js";
+import {Grupo, Usuario} from "../models/index.js";
 import Categoria from "../models/Categoria.js";
+import Meeti from "../models/Meeti.js";
+import moment from "moment";
+import {Op} from "sequelize";
 
-const findCategoriaById = async (req, res) => {
+const findCategoriaById = async (req, res, next) => {
     const id = req.params.id;
-    const categoria = await Categoria.findByPk(id);
-    const gruposPorCategoria = await Grupo.findAll({
-        where: {
-            categoria_id: id
-        }
+    const fechaActual = moment().format("YYYY-MM-DD");
+    const categoria = await Categoria.findOne({
+        where: { id: id },
+        attributes: ["id", "nombre"]
     });
+    if(!categoria){
+        res.redirect("/");
+        return next();
+    }
+
+    const meetisPorCategoria = await Meeti.findAll({
+        include: [
+            {model: Grupo, where: {categoria_id: categoria.id}},
+            {model: Usuario}
+        ],
+        where: {
+            fecha: { [Op.gt]: fechaActual}
+        }
+    })
 
     res.render("areaPublica/categorias/grupoPorCategoria", {
         nombrePagina: `Grupos de Categoria: ${categoria.nombre}`,
-        csrf: req.csrfToken()
+        csrf: req.csrfToken(),
+        meetis: meetisPorCategoria,
+        moment
     });
 }
 
